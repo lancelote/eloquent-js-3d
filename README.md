c902f0f5e25658a077b2cdac87f210b61c4e4e42
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-11-25 21:52:24 +0300

    Lycanthrope Example

--- README.md
+++ README.md
@@ -16,8 +16,9 @@ My code for "[Eloquent JavaScript][1]" 3rd Edition by Marijn Haverbeke
     - [Exercise 3: Bean Counting](src/chapter03/bean_counting.js)
   - Chapter 4: Data Structures: Objects and Arrays
     - [Calculating PHI](src/chapter04/calculating_phi.js)
+    - [Lycanthrope Example](src/chapter04/lycanthrope.js)
   - Chapter 5: Higher-Order Functions
-    - [`some` function](src/chapter05/some_function.js)
+    - [`some` Function](src/chapter05/some_function.js)
   - Chapter 6: The Secret Life of Objects
   - Chapter 7: Project: A Robot
   - Chapter 8: Bugs and Errors
---
+++ src/chapter04/lycanthrope.js
@@ -0,0 +1,47 @@
+import { JOURNAL } from "./journal.js";
+
+const phi = table => {
+  //      11         00         10         01
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+      (table[0] + table[1]) *
+      (table[1] + table[3]) *
+      (table[0] + table[2])
+    );
+};
+
+const tableFor = (event, journal) => {
+  let table = [0, 0, 0, 0];
+  for (let entry of journal) {
+    let index = 0;
+
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+console.assert(tableFor("pizza", JOURNAL)[0] === 76);
+
+const journalEvents = journal => {
+  let events = [];
+  for (const entry of journal) {
+    for (const event of entry.events) {
+      if (!events.includes(event)) {
+        events.push(event);
+      }
+    }
+  }
+  return events;
+};
+
+console.assert(journalEvents(JOURNAL)[0] === "carrot");
+
+for (let event of journalEvents(JOURNAL)) {
+  let correlation = phi(tableFor(event, JOURNAL));
+  if (correlation > 0.1 || correlation < -0.1) {
+    console.log(`${event}: ${correlation}`);
+  }
+}
